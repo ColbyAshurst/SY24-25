018aa40122ac6736d7e82364caa18f3295d0e227
ColbyJack <Ciash155@learner.dvusd.org>
2024-09-10 09:14:53 -0700

    adding mine program to system

---
+++ MineSweeper/MineSweeper/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Form1 : Form
+    {
+        Button[] btnGrid = new Button[100];
+        tile[] tileGrid = new tile[100];
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+                {
+                btnGrid[i] = (Button)Controls["button" + (i + 1)];
+                tileGrid[i] = new tile(btnGrid[i]);  
+                btnGrid[i].BackColor = Color.Green;
+            }
+                for (int i = 0; i < 100; i++)
+                {
+                {
+                }
+            }
+        }
+                private Button GetButton(int r, int c)
+                {
+                return (Button)GetButton(r, c);
+                }
+                private int getIndex(Button b)
+                {
+                string tmp = b.Name.Substring(6);   
+                int  retVal = 0;
+                int.TryParse(tmp, out retVal);  
+                return retVal - 1;
+                }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getIndex (sender as Button);
+        }
+
+        private void button100_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            b.BackColor = Color.Red;  
+        }
+    }
+}
