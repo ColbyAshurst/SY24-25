4d1506d368ccdfc251dbfb1400ca538809a46cee
ColbyJack <Ciash155@learner.dvusd.org>
2025-01-14 08:54:39 -0700

    added binary project

---
+++ BinaryCodeProject2025/BinaryCodeProject2025/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BinaryCodeProject2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bit8 = new System.Windows.Forms.TextBox();
+            this.Bit7 = new System.Windows.Forms.TextBox();
+            this.Bit6 = new System.Windows.Forms.TextBox();
+            this.Bit5 = new System.Windows.Forms.TextBox();
+            this.Bit4 = new System.Windows.Forms.TextBox();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.Bit3 = new System.Windows.Forms.TextBox();
+            this.Bit2 = new System.Windows.Forms.TextBox();
+            this.Bit1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Bit8
+            // 
+            this.Bit8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit8.Location = new System.Drawing.Point(2, 2);
+            this.Bit8.MaxLength = 1;
+            this.Bit8.Name = "Bit8";
+            this.Bit8.Size = new System.Drawing.Size(100, 34);
+            this.Bit8.TabIndex = 0;
+            this.Bit8.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit7
+            // 
+            this.Bit7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit7.Location = new System.Drawing.Point(108, 2);
+            this.Bit7.MaxLength = 1;
+            this.Bit7.Name = "Bit7";
+            this.Bit7.Size = new System.Drawing.Size(100, 34);
+            this.Bit7.TabIndex = 1;
+            this.Bit7.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit6
+            // 
+            this.Bit6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit6.Location = new System.Drawing.Point(214, 2);
+            this.Bit6.MaxLength = 1;
+            this.Bit6.Name = "Bit6";
+            this.Bit6.Size = new System.Drawing.Size(100, 34);
+            this.Bit6.TabIndex = 2;
+            this.Bit6.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit5
+            // 
+            this.Bit5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit5.Location = new System.Drawing.Point(320, 2);
+            this.Bit5.MaxLength = 1;
+            this.Bit5.Name = "Bit5";
+            this.Bit5.Size = new System.Drawing.Size(100, 34);
+            this.Bit5.TabIndex = 3;
+            this.Bit5.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit4
+            // 
+            this.Bit4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit4.Location = new System.Drawing.Point(426, 2);
+            this.Bit4.MaxLength = 1;
+            this.Bit4.Name = "Bit4";
+            this.Bit4.Size = new System.Drawing.Size(100, 34);
+            this.Bit4.TabIndex = 4;
+            this.Bit4.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(12, 73);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(44, 16);
+            this.totalLabel.TabIndex = 5;
+            this.totalLabel.Text = "label1";
+            // 
+            // Bit3
+            // 
+            this.Bit3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit3.Location = new System.Drawing.Point(532, 2);
+            this.Bit3.Name = "Bit3";
+            this.Bit3.Size = new System.Drawing.Size(100, 34);
+            this.Bit3.TabIndex = 6;
+            this.Bit3.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit2
+            // 
+            this.Bit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit2.Location = new System.Drawing.Point(638, 2);
+            this.Bit2.Name = "Bit2";
+            this.Bit2.Size = new System.Drawing.Size(100, 34);
+            this.Bit2.TabIndex = 7;
+            this.Bit2.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit1
+            // 
+            this.Bit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit1.Location = new System.Drawing.Point(744, 2);
+            this.Bit1.Name = "Bit1";
+            this.Bit1.Size = new System.Drawing.Size(100, 34);
+            this.Bit1.TabIndex = 8;
+            this.Bit1.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 448);
+            this.Controls.Add(this.Bit1);
+            this.Controls.Add(this.Bit2);
+            this.Controls.Add(this.Bit3);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.Bit4);
+            this.Controls.Add(this.Bit5);
+            this.Controls.Add(this.Bit6);
+            this.Controls.Add(this.Bit7);
+            this.Controls.Add(this.Bit8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Bit8;
+        private System.Windows.Forms.TextBox Bit7;
+        private System.Windows.Forms.TextBox Bit6;
+        private System.Windows.Forms.TextBox Bit5;
+        private System.Windows.Forms.TextBox Bit4;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox Bit3;
+        private System.Windows.Forms.TextBox Bit2;
+        private System.Windows.Forms.TextBox Bit1;
+    }
+}
+
---
+++ BinaryCodeProject2025/BinaryCodeProject2025/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryCodeProject2025
+{
+    public partial class Form1 : Form
+    {
+        int total = 0;
+        int[] Bits = new int[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Calc()
+        {
+            total = 0;
+        if (Bits[0] != 0) total+=1;
+        if (Bits[1] != 0) total += 2;
+        if (Bits[2] != 0) total += 4;
+        if (Bits[3] != 0) total += 8;
+        if (Bits[4] != 0) total += 16;
+        if (Bits[5] != 0) total += 32;
+        if (Bits[6] != 0) total += 64;
+        if (Bits[7] != 0) total += 128;
+        totalLabel.Text = total.ToString();
+        }
+
+        private void Bit1_TextChanged(object sender, EventArgs e)
+        {
+            if (Bit1.Text == "1") Bits[0] = 1;
+            else Bits[0] = 0;
+            //Bit1
+            if (Bit2.Text == "1") Bits[1] = 1;
+            else Bits[1] = 0;
+            //Bit2
+            if (Bit3.Text == "1") Bits[2] = 1;
+            else Bits[2] = 0;
+            //Bit3
+            if (Bit4.Text == "1") Bits[3] = 1;
+            else Bits[3] = 0;
+            //Bit4
+            if (Bit5.Text == "1") Bits[4] = 1;
+            else Bits[4] = 0;
+            //Bit5
+            if (Bit6.Text == "1") Bits[5] = 1;
+            else Bits[5] = 0;
+            //Bit6
+            if (Bit7.Text == "1") Bits[6] = 1;
+            else Bits[6] = 0;
+            //Bit7
+            if (Bit8.Text == "1") Bits[7] = 1;
+            else Bits[7] = 0;
+            //Bit8
+
+            Calc();
+        }
+    }
+}
